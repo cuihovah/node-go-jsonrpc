a96bbeede4ffe4da4ca1b9a4d59f8afc2b426593
cuiyunfeng <cuihovah@gmail.com>
2020-05-16 22:13:09 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # node-go-jsonrpc
 The client jsonrpc for golang
+
+## Install
+```bash
+> npm install node-go-jsonrpc
+```
+
+## Usage
+### Server
+```go
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+}
+
+func main() {
+	wait := make(chan bool)
+	arith := new(Arith)
+	rpc.Register(arith)
+	l, _ := net.Listen("tcp", ":PORT")
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		go jsonrpc.ServeConn(conn)
+	}
+	<- wait
+}
+```
+
+### Client
+```js
+const jsonrpc = require('node-go-jsonrpc')
+
+// create a client
+const client = new jsonrpc.JSONRPC("YOUR HOST", "YOUR PORT")
+
+// call JSON-RPC server
+const method = 'Arith.Multiply'
+const params = [{A:7,B:8}]
+const ret = await client.call(method, params)
+```
---
+++ main.js
@@ -0,0 +1,45 @@
+const net = require('net')
+
+class JSONRPC {
+    constructor(host, port) {
+        this.count = 0
+        this.client = new net.Socket()
+        this.host = host
+        this.port = port
+    }
+    async call(method, data) {
+        const body = {
+            method: method, 
+            params: data, 
+            id: this.count++
+        }
+        return new Promise((resolve, reject) => {
+            this.client.connect(this.port, this.host, () => {
+                this.client.write(JSON.stringify(body));
+            })
+            this.client.on('data', (data) => {
+                try {
+                    const ret = JSON.parse(data)
+                    if (ret.error !== null) {
+                        this.client.destroy()
+                        reject(new Error(ret.error))
+                    } else {
+                        this.client.destroy()
+                        resolve(ret)
+                    }
+                } catch(err) {
+                    reject(err)
+                }
+            })
+            this.client.on('error', (err) => {
+                this.client.destroy()
+                reject(err)
+            })
+        })
+    }
+}
+
+module.exports = JSONRPC
+
+
+
